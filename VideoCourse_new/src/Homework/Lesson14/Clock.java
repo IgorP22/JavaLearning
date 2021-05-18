734ea247124916d9acc56dfc4ff2f94db11b9bd5
Igor Podverbnyj <igorp222222@gmail.com>
2021-05-18 17:58:10 +0300

    Homework14  correction

--- VideoCourse_new/src/Homework/Lesson14/Clock.java
+++ VideoCourse_new/src/Homework/Lesson14/Clock.java
@@ -4,23 +4,41 @@ package Homework.Lesson14;
 Урок 14 Классический цикл «for». Выражения «break» и «continue».
  */
 
+/*
+Исправление ошибок:
+Проверка перенесена в средний цикл, чтобы не было лишних бесполезных проверок
+Изменен вызов статического метода без использования имени класса
+
+Ошибки на работоспособность программы не влияют, но делают лишние действия
+
+*/
+
 public class Clock {
 
     public static void Timer() {
 
         OUTER:
         for (int hour = 0; hour <= 6; hour++) {
-            INNER1:
+            MIDDLE:
             for (int minute = 0; minute < 60; minute++) {
-                INNER2:
+
+
+                if (hour > 1 && minute != 0 && minute % 10 == 0) {
+                    System.out.println("Выход из метода");
+                    break OUTER;
+                }
+
+                INNER:
                 for (int second = 0; second < 60; second++) {
 
-                    // просто для корректного вывода времени в формато 00:00:00
-                    if (hour < 10) {
-                        System.out.print("0" + hour + ":");
-                    } else {
-                        System.out.print(hour + ":");
+                    if (second * hour > minute) {
+                        System.out.println("Переходим на следующую минуту");
+                        continue MIDDLE;
                     }
+
+                    // просто для корректного вывода времени в формато 00:00:00
+
+                    System.out.print("0" + hour + ":");
                     if (minute < 10) {
                         System.out.print("0" + minute + ":");
                     } else {
@@ -34,26 +52,25 @@ public class Clock {
 
 
 
-                    if (hour > 1 && minute!=0 &&  minute % 10 == 0) {
+                    /*if (hour > 1 && minute!=0 &&  minute % 10 == 0) {
                         System.out.println("Выход из метода");
-                        break OUTER;
-                    }
+                        break OUTER;*/
 
-                    if (second * hour > minute) {
+
+                    /*if (second * hour > minute) {
                         System.out.println("Переходим на следующую минуту");
-                        continue INNER1;
-                        // break INNER2;  можем использовать просто break внутреннего цикла
-                    }
-
-
+                        continue INNER1;*/
+                    // break INNER2;  можем использовать просто break внутреннего цикла
                 }
+
+
             }
         }
 
     }
 
     public static void main(String[] args) {
-        Clock.Timer();
+        Timer();
     }
 
 }
