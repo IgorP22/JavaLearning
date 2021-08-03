4d3ba689de6ce7ab835460d5c720cf35bb506086
Igor Podverbnyj <igorp222222@gmail.com>
2021-08-03 14:00:12 +0300

    Ternary operators

---
+++ VideoCourse_new/src/Homework/Lesson21/Test1.java
@@ -0,0 +1,18 @@
+package Homework.Lesson21;
+
+/*
+Урок 21 Понятие «garbage collection».
+
+
+ */
+
+public class Test1 {
+    public static void main(String[] args) {
+        // тернарные операторы
+        int a=7;
+        System.out.println((a>5)?7:10); //условие?правда:ложь
+        System.out.println(a>2?a<5?3:6:9); // в сложных тернарных операторах проще расставить скобки a>2?(a<5?3:6):9
+
+
+    }
+}
