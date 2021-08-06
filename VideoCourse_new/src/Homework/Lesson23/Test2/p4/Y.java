66511a32f085dbaa1accb63b282e1e6e9b0ec372
Igor Podverbnyj <igorp222222@gmail.com>
2021-08-06 18:08:59 +0300

    Homework 23 correction

--- VideoCourse_new/src/Homework/Lesson23/Test2/p4/Y.java
+++ VideoCourse_new/src/Homework/Lesson23/Test2/p4/Y.java
@@ -1,38 +1,38 @@
-package Homework.Lesson23.Test2.p4;
-import Homework.Lesson23.p3.*;
-
-//Test 2
-//Ответ: Результат работы будет -
-// 'Y'
-// 'Y'
-// 'Y'
-
-// Не верно: run-time error при вызове x.abc
-// abc() has protected access in Homework.Lesson23.p3.X
-// какаято фигня с этим, если меняем модификатор на public (запускаем) а потом на protected (запускаем) все запускается и выводит как написано
-// а если на privat, а потом обратно на protected не работает
-
-public class Y extends X {
-    public void abc () {
-        System.out.println('Y');
-    }
-
-    public void def () {
-        Y y = new Y();
-        y.abc();
-    }
-
-    public void ghi() {
-        X x = new Y();
-        x.abc ();
-    }
-
-    public static void main(String[] args) {
-        Y a = new Y();
-        a.abc();
-        a.def();
-        a.ghi();
-    }
-
-
-}
+//package Homework.Lesson23.Test2.p4;
+//import Homework.Lesson23.p3.*;
+//
+////Test 2
+////Ответ: Результат работы будет -
+//// 'Y'
+//// 'Y'
+//// 'Y'
+//
+//// Не верно: run-time error при вызове x.abc
+//// abc() has protected access in Homework.Lesson23.p3.X
+//// какаято фигня с этим, если меняем модификатор на public (запускаем) а потом на protected (запускаем) все запускается и выводит как написано
+//// а если на privat, а потом обратно на protected не работает
+//
+//public class Y extends X {
+//    public void abc () {
+//        System.out.println('Y');
+//    }
+//
+//    public void def () {
+//        Y y = new Y();
+//        y.abc();
+//    }
+//
+//    public void ghi() {
+//        X x = new Y();
+//        x.abc ();
+//    }
+//
+//    public static void main(String[] args) {
+//        Y a = new Y();
+//        a.abc();
+//        a.def();
+//        a.ghi();
+//    }
+//
+//
+//}
